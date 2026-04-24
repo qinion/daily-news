de0303edbc9edb98036ae33234de145279c8d2a3
Jesse <65962552+qinion@users.noreply.github.com>
2026-04-24 15:06:26 +0800

    Implement news fetching and mailing functionality

---
+++ main.py
@@ -0,0 +1,14 @@
+from news_crawler import get_top_news
+from mailer import send_mail
+
+if __name__ == '__main__':
+    news_list = get_top_news()
+    if not news_list:
+        print("No news to send.")
+        exit(0)
+    content = "\n\n".join([str(n) for n in news_list])
+    send_mail(
+        to_addr="619488300@qq.com",
+        subject="每日科技新闻日报",
+        content=content
+    )
